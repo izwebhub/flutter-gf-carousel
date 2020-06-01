8db66ddc741bd8ba623c47217797ad78da303c74
Joram Kimata <joramkimata@gmail.com>
2020-06-01 10:10:00 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/sliderapp2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sliderapp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sliderapp2/app.dart';
+
+void main() => runApp(MyApp());
+
